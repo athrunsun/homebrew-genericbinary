f7770a61301f4f82b181b36aca62e361d6407fb3
Bruce Sun <brucesun@adobe.com>
2018-03-21 09:42:23 +0800

    Upgrade yarn-binary to version 1.5.1

--- Formula/yarn-binary.rb
+++ Formula/yarn-binary.rb
@@ -1,7 +1,7 @@
 class YarnBinary < Formula
     desc "JavaScript package manager, binary install (without any dependencies)."
     homepage "https://github.com/yarnpkg/yarn"
-    version "1.3.2"
+    version "1.5.1"
 
     option "with-taobao-mirror", "Download tarball from https://npm.taobao.org/mirrors/yarn/, use this option if you're in mainland China for a faster downloading speed."
 
@@ -11,7 +11,7 @@ class YarnBinary < Formula
         url "https://github.com/yarnpkg/yarn/releases/download/v#{version}/yarn-v#{version}.tar.gz"
     end
 
-    sha256 "6cfe82e530ef0837212f13e45c1565ba53f5199eec2527b85ecbcd88bf26821d"
+    sha256 "cd31657232cf48d57fdbff55f38bfa058d2fb4950450bd34af72dac796af4de1"
 
     bottle :unneeded
 
@@ -19,15 +19,15 @@ class YarnBinary < Formula
 
     def install
         libexec.install Dir["*"]
-        bin.install_symlink "#{libexec}/bin/yarn.js" => "yarn"
-        bin.install_symlink "#{libexec}/bin/yarn.js" => "yarnpkg"
+        bin.install_symlink("#{libexec}/bin/yarn.js" => "yarn")
+        bin.install_symlink("#{libexec}/bin/yarn.js" => "yarnpkg")
         inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
     end
 
     def caveats; <<~EOS
         This formula differs from the original one,
         as it doesn't have any dependencies,
-        and is downloading tarball from github or mirror site instead of official site.
+        and is able to download tarball from github or mirror site instead of official site.
         EOS
     end
 end
