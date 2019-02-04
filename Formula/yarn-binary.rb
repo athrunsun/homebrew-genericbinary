cddb9bbe1fb86584dfd50a9d3d9bee426ccb5f47
okampfer <okampfer@gmail.com>
2019-02-04 15:19:11 +0800

    Upgrade yarn-binary to version 1.13.0

--- Formula/yarn-binary.rb
+++ Formula/yarn-binary.rb
@@ -1,7 +1,7 @@
 class YarnBinary < Formula
     desc "JavaScript package manager, binary install (without any dependencies)."
     homepage "https://github.com/yarnpkg/yarn"
-    version "1.12.3"
+    version "1.13.0"
 
     option "with-taobao-mirror", "Download tarball from https://npm.taobao.org/mirrors/yarn, use this option if you're in mainland China for a faster downloading speed."
 
@@ -11,7 +11,7 @@ class YarnBinary < Formula
         url "https://github.com/yarnpkg/yarn/releases/download/v#{version}/yarn-v#{version}.tar.gz"
     end
 
-    sha256 "02cd4b589ec22c4bdbd2bc5ebbfd99c5e99b07242ad68a539cb37896b93a24f2"
+    sha256 "125d40ebf621ebb08e3f66a618bd2cc5cd77fa317a312900a1ab4360ed38bf14"
 
     bottle :unneeded
 
